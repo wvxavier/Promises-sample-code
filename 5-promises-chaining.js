fc2088681f1032a0a57e398b53e9cfe1a539b428
WOLTTER VINYCIUS CARVALHO XAVIER <50013554+wvxavier@users.noreply.github.com>
2019-07-04 21:02:22 +1000

    Add files via upload

---
+++ 5-promises-chaining.js
@@ -0,0 +1,23 @@
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+add(1, 2).then((sum) => {
+    console.log(sum)
+
+    add(sum, 7).then((sum2) => {
+        console.log(sum2)
+
+        add(sum2, 11).then((sum3) => {
+            console.log(sum3)
+        })
+
+    })
+    
+}).catch((error) => {
+    console.log(error)
+})
